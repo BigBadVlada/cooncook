db869026e3909d309c6ebe9a66c79668cb09e666
Vladimir Jovanovic <vladimir.jovanovic@bookaboat.co>
2015-02-03 01:19:36 +0100

    working copy

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@
 
 $(document).ready(function() {
   $('#title1').click(function() {
+    $('#title1 h5').toggleClass('arrow-up arrow-down');
     $('#list1').slideToggle('fast');
   });
   $('#title2').click(function() {
+    $('#title2 h5').toggleClass('arrow-up arrow-down');
     $('#list2').slideToggle('fast');
   });
   $('#title3').click(function() {
+    $('#title3 h5').toggleClass('arrow-up arrow-down');
     $('#list3').slideToggle('fast');
   });
   $('#title4').click(function() {
+    $('#title4 h5').toggleClass('arrow-down arrow-up');
     $('#list4').slideToggle('fast');
   });
   $('#myCarousel').carousel();
